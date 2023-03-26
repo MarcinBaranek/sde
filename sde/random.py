fea3ddc4e29fe263276e8f1b859c061aa0a2a25a
MarcinBaranek <baranekmarcin47@gmail.com>
2023-03-26 11:05:49 +0200

    Add uniform generator and tests to random

--- sde/random.py
+++ sde/random.py
@@ -1,8 +1,33 @@
 from numba import cuda
 from numba.core.types import float16
 from numba.cuda.random import (
-    xoroshiro128p_normal_float32, xoroshiro128p_normal_float64
+    xoroshiro128p_normal_float32, xoroshiro128p_normal_float64,
+    xoroshiro128p_uniform_float32, xoroshiro128p_uniform_float64
 )
+
+
+@cuda.jit(device=True)
+def gen_uniform_16(start, end, state):
+    thread_id = cuda.grid(1)
+    return start + (end - start) * float16(
+        xoroshiro128p_uniform_float32(state, thread_id)
+    )
+
+
+@cuda.jit(device=True)
+def gen_uniform_32(start, end, state):
+    thread_id = cuda.grid(1)
+    return start + (end - start) * xoroshiro128p_uniform_float32(
+        state, thread_id
+    )
+
+
+@cuda.jit(device=True)
+def gen_uniform_64(start, end, state):
+    thread_id = cuda.grid(1)
+    return start + (end - start) * xoroshiro128p_uniform_float64(
+        state, thread_id
+    )
 
 
 @cuda.jit(device=True)
@@ -28,3 +53,25 @@ def gen_normal_64(place_holder, state):
     for i in range(place_holder.shape[0]):
         for j in range(place_holder.shape[1]):
             place_holder[i, j] = xoroshiro128p_normal_float64(state, thread_id)
+
+
+def get_normal_generator(precision: str):
+    if precision == 'float16':
+        return gen_normal_16
+    elif precision == 'float32':
+        return gen_normal_32
+    elif precision == 'float64':
+        return gen_normal_64
+    else:
+        raise ValueError(f'Unknown precision: {precision}')
+
+
+def get_uniform_generator(precision: str):
+    if precision == 'float16':
+        return gen_uniform_16
+    elif precision == 'float32':
+        return gen_uniform_32
+    elif precision == 'float64':
+        return gen_uniform_64
+    else:
+        raise ValueError(f'Unknown precision: {precision}')
--- tests/unit_tests/test_random.py
+++ tests/unit_tests/test_random.py
@@ -1,24 +1,58 @@
 import numpy as np
+import numpy.testing as npt
 import pytest
+from numba import cuda
 
 from sde import KernelWrapper, State
-from sde.random import gen_normal_16, gen_normal_32, gen_normal_64
-
-from .utils import precision
-
-generators = {
-    'float16': gen_normal_16,
-    'float32': gen_normal_32,
-    'float64': gen_normal_64,
-}
+from sde.random import get_normal_generator, get_uniform_generator
+from sde.utils import get_thread_id
+from .utils import tolerance
 
 
 @pytest.mark.parametrize('shape', [(3, 4), (1, 2), (2, 1)])
-def test_gen_something(precision, shape: tuple[int]):
+def test_gen_normal_fill_array(precision, shape: tuple[int]):
     a = np.zeros(shape=shape)
     kernel = KernelWrapper(
-        generators[precision], precision, (0,), device=True, n_args=1,
-        state=State(n=shape[0] * shape[1], seed=7)
+        get_normal_generator(precision), precision, (0,), device=True,
+        n_args=1, state=State(n=shape[0] * shape[1], seed=7)
     )
     kernel[1, 1](a)
     assert (a**2).sum() > 2
+
+
+def test_gen_normal_match_values(precision):
+    a = np.zeros(shape=(1, 4))
+    kernel = KernelWrapper(
+        get_normal_generator(precision), precision, (0,), device=True,
+        n_args=1, state=State(n=4, seed=7)
+    )
+    kernel[1, 1](a)
+    npt.assert_allclose(
+        a, [[0.61972869, -1.33773295, -0.44804679, -0.94795856]],
+        atol=tolerance[precision]
+    )
+
+
+class TestGenUniform:
+    @staticmethod
+    def get_test_kernel(generator):
+        @cuda.jit
+        def kernel(vector, start, end, state):
+            pos = get_thread_id()
+            if pos < vector.size:
+                vector[pos] = generator(start, end, state)
+
+        return kernel
+
+    def test_gen_uniform(self, precision):
+        a = np.zeros(shape=(4,))
+        kernel = KernelWrapper(
+            self.get_test_kernel(get_uniform_generator(precision)),
+            precision, (0,), device=False, state=State(n=10, seed=7)
+        )
+        kernel[2, 2](a, 0, 1)
+        assert (a**2).sum() > 1.5
+        npt.assert_allclose(
+            a, [0.7796595, 0.09631481, 0.97113137, 0.53298037],
+            atol=tolerance[precision]
+        )
